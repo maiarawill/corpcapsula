990f4fe5b5ed8c3fe9edbd7a5e1c51f53ed3278d
Maiara Cristina Will <maiaracwill@gmail.com>
2023-02-07 14:23:44 -0300

    first maiaras commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1>Calculadora P.A. / P.G.</h1>
+
+        <h4>O que você quer calcular?</h4>
+      </div>
+
+      <div>
+        <label for='pa'>
+          <input type="radio" id="pa" value="pa" class="checkbox"></input>
+          P.A.
+        </label>
+
+        <label for='pg'>
+          <input type="radio" id="pg" value="pg" class="checkbox"></input>
+          P.G.
+        </label>
+      </div>
+
+      <div>
+        <label for='pg'>
+          Razão
+          <input type="text" id="pg" value="pg"  class="valoresRecebidos"></input>
+        </label>
+        
+        <label for='pg'>
+          Primeiro número
+          <input type="text" id="pg" value="pg"  class="valoresRecebidos"></input>
+        </label>
+
+        <label for='pg'>
+          Último número
+          <input type="text" id="pg" value="pg"  class="valoresRecebidos"></input>
+        </label>
+
+        <label for='pg'>
+          Número de termos
+          <input type="text" id="pg" value="pg"  class="valoresRecebidos"></input>
+        </label>
+      </div>
+
+      <div>
+        <h3>Razão:{}</h3>
+
+        <h3>Termo geral:{}</h3>
+
+        <h3>Soma Finita:{}</h3>
+      </div>
     </div>
   );
 }
